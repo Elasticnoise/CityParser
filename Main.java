069afef9a7b34e38abd2f993a22021951a300016
Vyacheslav Tretyakov <53997232+Elasticnoise@users.noreply.github.com>
2022-05-28 15:16:17 +0300

    Update Main.java
    change some logic

--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
 package com.company;
 
 import java.io.IOException;
+import java.util.List;
 
 import static com.company.CityUtils.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException { parse();}
+    public static void main(String[] args) throws IOException {
+        List<City> cities = parse();
+//        SortByCityName(cities);
+        CityAndDistrictSort(cities);
+        cities.forEach(System.out::println);
+    }
 }
