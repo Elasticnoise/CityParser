5345d4a1ccb588806a3f568aa23518429616f814
Vyacheslav Tretyakov <53997232+Elasticnoise@users.noreply.github.com>
2022-05-29 15:23:59 +0300

    Update CityUtils.java
    add cityInRegion

--- CityUtils.java
+++ CityUtils.java
@@ -4,7 +4,9 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.MessageFormat;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class CityUtils {
 
@@ -46,5 +48,43 @@ public class CityUtils {
     public static void CityAndDistrictSort(List<City> list) {
         Comparator<City> comparator = Comparator.comparing(City::getDistrict).thenComparing(City::getName);
         list.sort(comparator);
+    }
+
+    public static City findMaxPopulation(City[] cityArray) {
+        City maxPopulation = cityArray[0];
+        for (int i = 1; i < cityArray.length; i++) {
+            if (maxPopulation.getPopulation() < cityArray[i].getPopulation())
+                maxPopulation = cityArray[i];
+        }
+        return maxPopulation;
+    }
+
+    public static City recursiveFind(City[] cityArray, int max) {
+        if (cityArray.length > max) {
+            City next = recursiveFind(cityArray, max + 1);
+            return (cityArray[max].getPopulation() > next.getPopulation()) ? cityArray[max] : next;
+        } else {
+            return cityArray[0];
+        }
+    }
+
+    public static void cityInRegion(City[] cityArray) {
+        Map<String, Integer> counts = new HashMap<>();
+        for (City city : cityArray) {
+            if (counts.containsKey(city.getRegion())) {
+                counts.put(city.getRegion(), counts.get(city.getRegion()) + 1);
+            } else {
+                counts.put(city.getRegion(), 1);
+            }
+        }
+        for (Map.Entry<String, Integer> entry : counts.entrySet()) {
+            System.out.println(entry.getKey()+" - "+ entry.getValue());
+        }
+    }
+
+    public static void findCountCityByRegionV2(List<City> cities) {
+        Map<String, Integer> regions = new HashMap<>();
+        cities.forEach(city -> regions.merge(city.getRegion(), 1, Integer::sum));
+        regions.forEach((k, v) -> System.out.println(MessageFormat.format(" {0} = {1}", k, v)));
     }
 }
